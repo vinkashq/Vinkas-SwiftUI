f682d3db2d62a95a77b251a95f60ef556aa42479
Vinoth Kannan <svkn.87@gmail.com>
2024-12-12 12:45:53 +0530

    FEATURE: add new view for loading animation

---
+++ Sources/VinkasCore/LoadingAnimation.swift
@@ -0,0 +1,31 @@
+//
+//  LoadingAnimation.swift
+//  Vinkas
+//
+//  Created by Vinkas on 12/12/24.
+//
+
+import SwiftUI
+
+struct LoadingAnimation: View {
+    var body: some View {
+        Circle()
+            .trim(from: 0.5, to: 1.0)
+            .stroke(style: StrokeStyle(lineWidth: 2))
+            .frame(width: 100, height: 100)
+            .rotationEffect(.degrees(360))
+            .animation(Animation.linear(duration: 1).repeatForever())
+            .overlay(
+                Circle()
+                    .trim(from: 0, to: 0.5)
+                    .stroke(style: StrokeStyle(lineWidth: 2))
+                    .frame(width: 50, height: 50)
+                    .rotationEffect(.degrees(-360))
+                    .animation(Animation.linear(duration: 1).repeatForever())
+            )
+    }
+}
+
+#Preview {
+    LoadingAnimation()
+}
